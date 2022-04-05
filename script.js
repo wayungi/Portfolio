b983e2be361c86fe2fc718d31e3e18fcefd95d55
Francis <wayncis2@gmail.com>
2022-04-05 22:28:10 +0300

    Add modal window content

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const close  =  document.querySelector('.close')
 const nav = document.querySelector('.nav-holder')
 const navLinks = document.querySelectorAll('.nav-link')
 const hamburger = document.querySelector('.hamburger')
+const modelHook =  document.querySelectorAll('.modal-hook')
+
+
+const projectContent = {}
+
 
 close.addEventListener('click', (e) => {
     nav.classList.add('hide')
@@ -13,4 +18,23 @@ hamburger.addEventListener('click', (e) => {
 
 Array.from(navLinks).forEach(navlink => navlink.addEventListener('click', ()=>{
     nav.classList.add('hide')
+}))
+
+Array.from(modelHook).forEach(btn => btn.addEventListener('click', (e)=> {
+    const children = e.target.parentElement.children
+
+    console.log(children)
+
+    projectContent.title = children[`${0}`].textContent
+    projectContent.linkTitle = children[`${1}`].children[`${0}`].textContent
+    projectContent.linkRole = children[`${1}`].children[`${1}`].textContent
+    projectContent.linkYear = children[`${1}`].children[`${2}`].textContent
+    //projectContent.image = 
+    projectContent.paragraph = children[`${2}`].textContent
+    projectContent.live = 'https://wayungi.github.io/Portfolio/'
+    projectContent.sourceCode = ''
+
+    console.log(projectContent)
+
+    
 }))
