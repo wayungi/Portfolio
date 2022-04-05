dea578a9da8427728f6040eaa2108d584eedfb73
Francis <wayncis2@gmail.com>
2022-04-06 01:43:11 +0300

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const close  =  document.querySelector('.close')
-const nav = document.querySelector('.nav-holder')
-const navLinks = document.querySelectorAll('.nav-link')
-const hamburger = document.querySelector('.hamburger')
+const close = document.querySelector('.close');
+const nav = document.querySelector('.nav-holder');
+const navLinks = document.querySelectorAll('.nav-link');
+const hamburger = document.querySelector('.hamburger');
 
-close.addEventListener('click', (e) => {
-    nav.classList.add('hide')
-})
+close.addEventListener('click', () => {
+  nav.classList.add('hide');
+});
 
-hamburger.addEventListener('click', (e) => {
-    nav.classList.remove('hide')
-})
+hamburger.addEventListener('click', () => {
+  nav.classList.remove('hide');
+});
 
-Array.from(navLinks).forEach(navlink => navlink.addEventListener('click', ()=>{
-    nav.classList.add('hide')
-}))
+Array.from(navLinks).forEach((navlink) => navlink.addEventListener('click', () => {
+  nav.classList.add('hide');
+}));
