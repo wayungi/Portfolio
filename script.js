c4bba2f2148de9471bd76ccae458dfc83e116db2
Francis <wayncis2@gmail.com>
2022-04-05 11:57:15 +0300

    Add pointer cursor to hamburger

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const close  =  document.querySelector('.close')
+const nav = document.querySelector('.nav-holder')
+const navLinks = document.querySelectorAll('.nav-link')
+
+close.addEventListener('click', (e) => {
+    nav.classList.add('hide')
+
+})
+
+Array.from(navLinks).forEach(navlink => navlink.addEventListener('click', ()=>{
+    nav.classList.add('hide')
+}))
 
 console.log(close)
+console.log(nav)
