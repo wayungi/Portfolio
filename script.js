fcf3e9aeca36a7657e17fc84940558198d18b19a
Francis <wayncis2@gmail.com>
2022-04-05 12:02:37 +0300

    Clean code

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const close  =  document.querySelector('.close')
 const nav = document.querySelector('.nav-holder')
 const navLinks = document.querySelectorAll('.nav-link')
+const hamburger = document.querySelector('.hamburger')
 
 close.addEventListener('click', (e) => {
     nav.classList.add('hide')
+})
 
+hamburger.addEventListener('click', (e) => {
+    nav.classList.remove('hide')
 })
 
 Array.from(navLinks).forEach(navlink => navlink.addEventListener('click', ()=>{
     nav.classList.add('hide')
 }))
-
-console.log(close)
-console.log(nav)
