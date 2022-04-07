a72ec742622a075c347345354714dc191c1affbc
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-07 17:49:26 -0300

    Add handle for contact form in javascript

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const close = document.querySelector('.close');
 const nav = document.querySelector('.nav-holder');
 const navLinks = document.querySelectorAll('.nav-link');
 const hamburger = document.querySelector('.hamburger');
-
+const form = document.querySelector('.form')
 
 close.addEventListener('click', () => {
   nav.classList.add('hide');
