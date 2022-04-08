7f6c0bbbc49bded35b27e4af5ba53fe6c7bedaef
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-08 20:49:53 -0300

    create the modal window image

--- script.js
+++ script.js
@@ -102,7 +102,12 @@ Array.from(projectBtns).forEach((projectBtn) =>projectBtn.addEventListener('clic
   ul.appendChild(li3);
   taglineDiv.appendChild(ul)
   modalBody.appendChild(taglineDiv);
-
+  
+  const imageDiv = createTag('div')
+  imageDiv.classList.add('modal-img')
+  const imgTag = createTagWithAttributeOnly('img', ['src',project.featuredImage])
+  imageDiv.appendChild(imgTag)
+  modalBody.appendChild(imageDiv)
   modalWindow.classList.remove('close')
   
 }));
