76d2d54ed15205c1870bea0ed8fc0c84452060c8
Francis <wayncis2@gmail.com>
2022-04-09 21:41:55 +0300

    Fix lowercase email verification

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const close = document.querySelector('.close');
 const nav = document.querySelector('.nav-holder');
 const navLinks = document.querySelectorAll('.nav-link');
 const hamburger = document.querySelector('.hamburger');
-const form = document.querySelector('.form');
-const email = document.querySelector('.email');
+const form = document.forms['contactForm'];
+// const email = document.querySelector('.email');
 const errorDiv = document.querySelector('.error');
+
+
+console.log(form.elements)
+
+
 
 close.addEventListener('click', () => {
   nav.classList.add('hide');
@@ -19,19 +24,20 @@ Array.from(navLinks).forEach((navlink) => navlink.addEventListener('click', () =
 }));
 
 form.addEventListener('submit', (e) => {
-  const emailRegex = new RegExp('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/');
-
-  if (emailRegex.test(email)) {
-    e.preventDefault();
-    errorDiv.innerText = 'Email address must be valid and in lowercase';
+  e.preventDefault();
+  errorDiv.textContent = '';
+  Array.from(form.elements).forEach((element) => {
+    if (!element.validity.valid) {
+      if (element.validity.patternMismatch) {
+        errorDiv.textContent = 'Please use only lowercase characters.';
+      }
+    }
+  });
+  if (form.reportValidity()) {
+    form.submit();
   }
+
 });
 
 
-const emailField = document.querySelector('#email');
-const contactForm = document.querySelector('.form');
-// console.log(contactForm);
 
-contactForm.addEventListener('submit', () => {
-  console.log(contactForm.elements['user_email'])
-})
